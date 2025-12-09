6f2d760983ff20ba8fe57d75c2a730c5adfed2a9
SIMEKA <144261735+simeka99@users.noreply.github.com>
2025-12-09 09:09:29 +0700

    Update info-toko.js

--- js/info-toko.js
+++ js/info-toko.js
@@ -1,1 +1,10 @@
+window.CONFIG_CUSTOM = {
+    GROQ_API_KEY: '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',
+    DEFAULT_INFO_TOKO: {
+        nama: "TK BINTANG JAYA",
+        alamat: "Kp Cikawungading RT 4 Rw 1\nKec Cipatujah Kab Tasikmalaya",
+        telp: "0853 2264 8888",
+        footer: "Terima Kasih\nSemoga Membawa Berkah Aamiin"
+    }
+};
 
